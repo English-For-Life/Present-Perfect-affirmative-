1f6df8c6133178c68bf99962dbc8924d6a66df2a
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:54:57 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"My car ___ down.",
+        options:["has broke", "has broken", "have broken", "have broke"],
+        answer: 1
+    },
+    {
+     q:"They ___ all my questions.",
+        options:["have answered", "has answered", "have answer", "have answerd"],
+        answer: 0   
+    },
+    {
+        q:"I ___ Mike fo 10 years.",
+        options:["has known", "have knowen", "have known", "has knowen"],
+        answer: 2
+    },
+    {
+        q:"She ___ her keys.",
+        options:["has lose", "have lose", "have lost", "has lost"],
+        answer: 3
+    },
+    {
+        q:"The cat ___ all of its milk.",
+        options:["have drunk", "has drank", "has drunk", "have drank"],
+        answer: 2
+    },
+    {
+        q:"We ___ to Canada two times.",
+        options:["has been", "have been", "have being", "has being"],
+        answer: 1
+    },
+    {
+        q:"Where is Mike? - He ___ for a walk.",
+        options:["has gone", "has went", "have gone", "have went"],
+        answer: 0
+    },
+    {
+        q:"The students ___ their test.",
+        options:["has finishen", "has finished", "have finishen", "have finished"],
+        answer: 3
+    },
+    {
+        q:"The lesson ___ .",
+        options:["has began", "has begun", "have begun", "have began"],
+        answer: 1
+    },
+    {
+        q:"I ___ my mobile phone somewhere but I can't remember where.",
+        options:["have leave", "has left", "have left", "have leaved"],
+        answer: 2
+    }
+
+]
